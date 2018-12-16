0db7be09315a8c206a60269af1b7996b8d708c76
even <even366@qq.com>
2018-12-16 21:35:46 +0800

    数据结构栈

---
+++ stack-demo/src/main/java/com/even/service/StackService.java
@@ -0,0 +1,11 @@
+package com.even.service;
+
+public interface StackService {
+    /**
+     * 判断栈是否为空
+     * @return
+     */
+    boolean isEmpty();
+    //https://github.com/shinezejian/javaStructures/blob/master/src/com/zejian/structures/Stack/CalculateExpression.java
+
+}
---
+++ stack-demo/src/main/java/com/even/service/impl/StackServiceImpl.java
@@ -0,0 +1,4 @@
+package com.even.service.impl;
+
+public class StackServiceImpl {
+}
