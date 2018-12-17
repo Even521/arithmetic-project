25398a06cc8261dddd129b97fd4e3b7fba1961da
Even <286937628@qq.com>
2018-12-17 15:27:07 +0800

    string 方法

--- linked-list-demo/src/main/java/com/even/doubleLinkTest.java
+++ linked-list-demo/src/main/java/com/even/doubleLinkTest.java
@@ -16,17 +16,17 @@ public class doubleLinkTest {
         HeadDoubleILinkedList<String> list=new HeadDoubleILinkedList<>(letters);
 
         System.out.println("list.get(3)->"+list.get(3));
-        System.out.println("list:"+list.toString());
+        System.out.println("list:"+list.tostring());
 
         System.out.println("list.add(4,Y)—>"+list.add(0,"Y"));
-        System.out.println("list:"+list.toString());
+        System.out.println("list:"+list.tostring());
         System.out.println("list.add(Z)—>"+list.add("Z"));
-        System.out.println("list:"+list.toString());
+        System.out.println("list:"+list.tostring());
 
 
         System.out.println("list.contains(Z)->"+list.contains("Z"));
         System.out.println("list.set(4,P)-->"+list.set(4,"P"));
-        System.out.println("list:"+list.toString());
+        System.out.println("list:"+list.tostring());
 
 
         System.out.println("list.remove(6)-->"+list.remove(6));
