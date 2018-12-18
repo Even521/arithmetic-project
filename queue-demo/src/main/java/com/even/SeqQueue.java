5b55ab0d8de014a8afb991ea5e3929ac1b029fa6
Even <286937628@qq.com>
2018-12-18 10:37:11 +0800

    顺序队列

---
+++ queue-demo/src/main/java/com/even/SeqQueue.java
@@ -0,0 +1,167 @@
+package com.even;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+
+/**
+ * Created by Administrator on 2018/12/18 0018.
+ *
+ * @author even
+ * 顺序队列 数组
+ * 只是伪代码，便于理解队列基本实现原理，并没有实现线程安全不能用于实际应用中。
+ */
+public class SeqQueue<T> implements IQueue<T>, Serializable {
+    /**
+     * 数组默认初始值10
+     */
+    private static final int DEFAULT=10;
+    /**
+     * 数组
+     */
+    private T data[];
+    /**
+     * 队头元素下标值
+     */
+    private int front;
+    /**
+     * 指向下一个入队的下标
+     */
+    private int rear;
+    /**
+     * 队列大小
+     */
+    private int size;
+
+    public SeqQueue(){
+        data=(T[]) new Object[DEFAULT];
+        front=rear=0;
+    }
+    public SeqQueue(int capacity){
+        data= (T[]) new Object[capacity];
+        front=rear=0;
+    }
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * 当front==rear时，队列为null
+     * @return
+     */
+    @Override
+    public boolean isEmpty() {
+        return front==rear;
+    }
+
+    /**
+     * 元素入队,成功true否则为false
+     * @param data
+     * @return
+     */
+    @Override
+    public boolean add(T data) {
+        //判断队列是否满队，满了则要扩容
+        if(this.front==(this.rear+1)%this.data.length){
+           this.ensureCapacity(this.data.length*2+1);
+        }
+        insertData(data);
+        return true;
+    }
+    /**
+     * 扩容的方法
+     * @param capacity
+     */
+    public void ensureCapacity(int capacity) {
+        //如果需要拓展的容量比现在数组的容量还小,则无需扩容
+        if (capacity<size) {
+            return;
+        }
+        T[] old = data;
+        data= Arrays.copyOf(old,capacity);
+        //恢复front,rear指向
+        this.front=0;
+        this.rear=0;
+    }
+
+    @Override
+    public boolean offer(T data) {
+        if (data==null) {
+            throw new NullPointerException("The data can\'t be null");
+        }
+        //队满抛出异常
+        if (this.front==(this.rear+1)%this.data.length){
+            throw new IllegalArgumentException("The capacity of SeqQueue has reached its maximum");
+        }
+
+        //添加data
+        insertData(data);
+        return true;
+
+    }
+
+    private void insertData(T data){
+        this.data[this.rear]=data;
+        //更新rear指向下一个空元素的位置
+        this.rear=(this.rear+1)%this.data.length;
+        size++;
+    }
+
+    /**
+     * 返回队头元素,不执行删除操作,若队列为空,返回null
+     * @return
+     */
+    @Override
+    public T peek() {
+        return data[front];
+    }
+
+    /**
+     * 返回队头元素,不执行删除操作,若队列为空,抛出异常:NoSuchElementException
+     * @return
+     */
+    @Override
+    public T element() {
+        if(isEmpty()){
+            throw new NoSuchElementException("The SeqQueue is empty");
+        }
+        return peek();
+    }
+    /**
+     * 出队,执行删除操作,返回队头元素,若队列为空,返回null
+     * @return
+     */
+    @Override
+    public T poll() {
+        T temp=this.data[this.front];
+        this.front=(this.front+1)%this.data.length;
+        size--;
+        return temp;
+    }
+    /**
+     * 出队,执行删除操作,若队列为空,抛出异常:NoSuchElementException
+     * @return
+     */
+    @Override
+    public T remove() {
+        if (isEmpty()){
+            throw new NoSuchElementException("The SeqQueue is empty");
+        }
+        return poll();
+    }
+
+    /**
+     * 清空队列
+     */
+    @Override
+    public void clearQueue() {
+        for (int i=this.front; i!=this.rear ; i=(i+1)%this.data.length) {
+           data[i] = null;
+        }
+        //复位
+        this.front=this.rear=0;
+        size=0;
+    }
+}
---
+++ queue-demo/src/main/java/com/even/SeqQueueTest.java
@@ -0,0 +1,26 @@
+package com.even;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+/**
+ * Created by Administrator on 2018/12/18 0018.
+ *
+ * @author even
+ */
+public class SeqQueueTest {
+    @Test
+    public void seqQueueTest(){
+        SeqQueue<String> stringSeqQueue=new SeqQueue<>();
+        stringSeqQueue.offer("A");
+        stringSeqQueue.offer("B");
+        stringSeqQueue.offer("C");
+        System.out.println("队列大小-> "+stringSeqQueue.size());
+        IntStream.range(0,stringSeqQueue.size())
+                .mapToObj(
+                        i->stringSeqQueue.poll()).forEach(
+                           System.out::println
+        );
+    }
+}
